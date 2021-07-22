5c1c3d5ba7732f9c182b39683a081a3e7a6d197f
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2021-07-23 00:57:23 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "=================================================" | lolcat
-echo -e "#         Premium Auto Script By Horasss        #" | lolcat
+echo -e "#         Premium Auto Script.                  #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
@@ -11,11 +11,8 @@ echo -e "# Build Up By Tatsumaru                         #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# Thanks To                                     #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# Allah                                  		 #" | lolcat
+echo -e "# Allah                                         #" | lolcat
 echo -e "# My Family                                     #" | lolcat
-echo -e "# Google                                        #" | lolcat
-echo -e "# Admin Portal & White Vps                      #" | lolcat
-echo -e "# VPNSTORES.NET                                 #" | lolcat
 echo -e "#                                               #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "=================================================" | lolcat
