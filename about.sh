61059a829aaa18261346c8ee6667e50fc16b85fa
Nanda Gunawan <nandakanaksasak@gmail.com>
2021-07-29 13:34:10 +0700

    My Script 2021

--- about.sh
+++ about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "=================================================" | lolcat
-echo -e "#         Premium Auto Script.                  #" | lolcat
+echo -e "#            Premium Auto Script.               #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
@@ -13,6 +13,5 @@ echo -e "# Thanks To                                     #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# Allah                                         #" | lolcat
 echo -e "# My Family                                     #" | lolcat
-echo -e "#                                               #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "=================================================" | lolcat
